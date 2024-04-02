68037645d7ab6d8da0d92716247a321fa5db287b
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-04-03 01:04:37 +0530

    Upload Images,From Data on Firebase And fetching it..

--- src/component/auctions/Body.js
+++ src/component/auctions/Body.js
@@ -1,10 +1,10 @@
 import React, { useContext, useState } from 'react';
 import { Alert } from 'react-bootstrap';
 import { AuthContext } from '../../context/AuthContext';
-// import useFirestore from '../../hooks/useFirestore';
+import useFirestore from '../../hooks/useFirestore';
 import AddAuction  from './AddAuction';
-// import AuctionCard from './AuctionCard';
-// import ProgressBar from './ProgressBar';
+import AuctionCard from './AuctionCard';
+import ProgressBar from './ProgressBar';
 
 
  const AuctionBody = () => {
@@ -20,14 +20,14 @@ import AddAuction  from './AddAuction';
         {globalMsg && <Alert variant="info">{globalMsg}</Alert>}
 
         {currentUser && <AddAuction setAuction={setAuction} />}
-{/* 
+
         {docs && (
           <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
             {docs.map((doc) => {
               return <AuctionCard item={doc} key={doc.id} />;
             })}
           </div>
-        )} */}
+        )}
       </div>
     </div>
   );
--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useState } from 'react';
+import { firestoreApp, storageApp, timestamp } from '../config/firebase';
 
-const useStorage = () => {
-  return (
-    <div>useStorage</div>
-  )
-}
+const useStorage = (data) => {
+  const [progress, setProgress] = useState(0);
+  const [isCompleted, setIsCompleted] = useState(null);
 
-export default useStorage
+  useState(() => {
+    const storageRef = storageApp.ref(data.itemImage.name);
+    const collectionRef = firestoreApp.collection('auctions');
+
+    storageRef.put(data.itemImage).on(
+      'state_changed',
+      (snap) => {
+        let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+        setProgress(percentage);
+      },
+      (err) => {
+        console.log(err);
+      },
+      async () => {
+        const imgUrl = await storageRef.getDownloadURL();
+        const createdAt = timestamp();
+        delete data.itemImage;
+        await collectionRef.add({ ...data, createdAt, imgUrl });
+        setIsCompleted(true);
+      }
+    );
+  }, [data]);
+
+  return { progress, isCompleted };
+};
+
+export default useStorage;
