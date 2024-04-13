9c992075079110a737b34dfc3da00542855468b1
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-04-13 20:00:46 +0530

    Adding Photo and some quotes on Testimonials..

--- src/component/pages/Testimonials/data.js
+++ src/component/pages/Testimonials/data.js
@@ -1,51 +1,27 @@
 const people = [
   {
     id: 1,
-    image: "/assets/priyanshu.jpg",
+    image: "/assets/amit.jpg",
     name: "Amit Katoch",
-    title: "Research Paper",
+    title: "Project Guider",
     quote:
-      "Since 2002, I've steered Kumar Fertilizer as India's top online agri-supplier. We specialize in seeds, fertilizers, pesticides, and more, committed to quality and affordability. Our mission is agricultural excellence, empowering farmers with reliable products and unwavering customer support. Kumar Fertilizer is more than a business; it's a legacy of prosperity in every field. ",
+      "As the project guide for Bidster, I provided strategic direction, advised on feature development, facilitated team collaboration, and offered mentorship throughout the project's lifecycle.",
   },
   {
     id: 2,
     image: "/assets/priyanshu.jpg",
     name: "Priyanshu Singh",
-    title: "Design nd Backend",
+    title: "UI & Backend",
     quote:
-      "As CEO and developer, I lead our tech journey, creating innovative websites for unparalleled online experiences. Specializing in cutting-edge development, we provide bespoke digital solutions. Beyond coding, leadership means shaping a digital legacy marked by creativity, functionality, and client satisfaction. Our mission: delivering standout websites that reflect excellence and tech advancement, leaving a lasting impact. ",
+      "As the UI and backend developer for Bidster, I meticulously crafted the user interface for optimal experience, ensured responsiveness, and managed backend development using Firebase Firestore for efficient data storage. Bidster's success reflects the dedication to delivering a seamless blend of aesthetics and functionality.",
   },
   {
     id: 3,
-    image: "/assets/priyanshu.jpg",
-    name: "Shri",
-    title: "Project Report",
-    quote:
-      "As Product Manager, I craft innovative products, tailoring solutions for diverse consumer needs. Leading strategic development, I aim for a legacy marked by innovation, usability, and customer satisfaction. Our mission: deliver standout products that resonate in the market, reflecting our commitment to excellence, making a lasting impact on consumers and industry standards.",
-  },
-  {
-    id: 4,
-    image: "/images/customer/Person4.JPG",
+    image: "/assets/harsh.jpeg",
     name: "Harsh Deep",
     title: "Frontend",
     quote:
-      "As Product Manager, I drive our company's innovation, creating solutions that resonate with diverse consumer needs. Beyond features, my focus is on a legacy of innovation, usability, and customer satisfaction. Our mission: deliver standout products, reflecting our commitment to excellence and making a lasting impact on consumers and industry standards. ",
-  },
-  {
-    id: 5,
-    image: "/images/customer/Person5.JPG",
-    name: "Axatsan",
-    title: "Research Paper",
-    quote:
-      "Kumar Fertilizer shop is a game-changer! Unbeatable quality and affordability have transformed my yields. Their dedicated customer support showcases genuine care. Kumar Fertilizer is my go-to for consistency and reliability. It's not just a product; it's the key to my field's success. Highly recommended for any farmer aiming for excellence in agriculture.",
-  },
-  {
-    id: 6,
-    image: "/images/customer/Person6.jpg",
-    name: "Amit Katoch",
-    title: "Project Guider",
-    quote:
-      "Kumar Fertilizer has transformed my farm with unparalleled quality and affordability. The remarkable boost in yields is a testament to its efficacy. Their customer support's dedication is admirable. Kumar Fertilizer is my constant, ensuring consistency and success in my fields. Highly recommend it for farmers seeking excellence in agriculture. ",
+      "As the frontend developer for Bidster, I crafted a visually appealing and intuitive user interface, optimizing for responsiveness to enhance user engagement and satisfaction, contributing significantly to Bidster's success.",
   },
 ];
 
