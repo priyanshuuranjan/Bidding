17458215107b717e086873c4eacdb0eb117b904f
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-05-24 02:58:18 +0530

    Adding Loader

--- src/App.js
+++ src/App.js
@@ -14,58 +14,71 @@ import Register from "./component/authentication/Register";
 import Home from "./component/Zoom/Home";
 import Video from "./component/Zoom/Video";
 import PaymentDetails from "./component/auctions/PaymentDetails";
-
+import HashLoader from "react-spinners/HashLoader";
 import { AuthProvider } from "./context/AuthContext";
-
-
+import "./App.css";
 
 const App = () => {
   const currentTheme = localStorage.getItem("current_theme");
-
   const [theme, setTheme] = useState(currentTheme || "light");
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    localStorage.setItem("current_theme", theme);
-  }, [theme]);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
 
   return (
     <AuthProvider>
-      <div className={`container ${theme}`}>
-        <Navbar theme={theme} setTheme={setTheme} />
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <>
-                <Hero />
-                <Feature subtitle="OUR PROGRAM" title="WHAT WE OFFER" />
-                {/* <Body /> */}
-                {/* <AuctionEnd/> */}
-                <Slider />
-              </>
-            }
-          ></Route>
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/contact" element={<Contact />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/login" element={<LoginSignup />} />
-          <Route exact path="/bids" element={<Body />} />
-          <Route exact path="/video" element={<Home />} />
-          <Route exact path="/room/:id" element={<Video />} />
-          <Route exact path="/endauction" element={<AuctionEnd />} />
-          <Route exact path="/PaymentDetails" element={<PaymentDetails />} />
-          
-        </Routes>
-        <Footer />
-      </div>
+      {loading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            flexDirection: "column",
+          }}
+        >
+          <HashLoader
+            color={"#36d7b7"}
+            size={100}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+          <h3>Loading...</h3>
+        </div>
+      ) : (
+        <div className={`container ${theme}`}>
+          <Navbar theme={theme} setTheme={setTheme} />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <Hero />
+                  <Feature subtitle="OUR PROGRAM" title="WHAT WE OFFER" />
+                  <Slider />
+                </>
+              }
+            ></Route>
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/contact" element={<Contact />} />
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/login" element={<LoginSignup />} />
+            <Route exact path="/bids" element={<Body />} />
+            <Route exact path="/video" element={<Home />} />
+            <Route exact path="/room/:id" element={<Video />} />
+            <Route exact path="/endauction" element={<AuctionEnd />} />
+            <Route exact path="/PaymentDetails" element={<PaymentDetails />} />
+          </Routes>
+          <Footer />
+        </div>
+      )}
     </AuthProvider>
   );
 };
 
 export default App;
-
-
-
-
-
-
